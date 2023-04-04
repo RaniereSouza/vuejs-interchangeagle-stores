e83b0bb48c09998291cefd7279688339ee5ed129
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-04-04 05:23:38 +0000

    Blank canvas for the demo app =)

---
+++ app/src/components/Todo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { onMounted } from 'vue'
+
+  const title = 'TODO App'
+  const description = 'Description: TBD'
+
+  onMounted(() => {
+    document.title = title
+  })
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p>{{ description }}</p>
+</template>
+
+<style scoped>
+  h1 {
+    color: cornflowerblue;
+  }
+</style>
